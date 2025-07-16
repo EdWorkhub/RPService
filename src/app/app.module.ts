2cf160b9a0bc8659c5d9ad6e18e0653c08cb069e
Ed <ed.ferris@workhub.com>
2025-07-16 17:45:35 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { GameComponent } from './menu/game/game.component';
+import { DisplayComponent } from './menu/game/display/display.component';
+import { HistoryComponent } from './menu/game/history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeaderComponent,
+    FooterComponent,
+    GameComponent,
+    DisplayComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
